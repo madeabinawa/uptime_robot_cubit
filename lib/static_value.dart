e65aca1430a62614498752689350a444f05961e4
I Made Dwija Abinawa <madeabinawa@gmail.com>
2021-01-26 17:21:08 +0800

    add static value requirements

---
+++ lib/static_value.dart
@@ -0,0 +1,3 @@
+const String apiKey = '';
+const String getAccountURL = 'https://api.uptimerobot.com/v2/getaccountdetails';
+const String getMonitorsURL = 'https://api.uptimerobot.com/v2/getmonitors';
