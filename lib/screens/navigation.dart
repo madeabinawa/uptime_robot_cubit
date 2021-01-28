02209e2e236d8b8b72e7d8948fa470fccbb9c7d9
I Made Dwija Abinawa <madeabinawa@gmail.com>
2021-01-28 11:44:21 +0800

    add navigation screen to navigate screen depends on cubit PageState

---
+++ lib/screens/navigation.dart
@@ -0,0 +1,18 @@
+part of 'screens.dart';
+
+// Navigation based on Page Cubit State
+class Navigation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<PageCubit, PageState>(
+      builder: (context, pageState) {
+        // if PageState cubit is PageWebview
+        // then set screen to Webview
+        if (pageState is PageWebview) {
+          return Webview(pageState.monitor);
+        }
+        return Homepage();
+      },
+    );
+  }
+}
